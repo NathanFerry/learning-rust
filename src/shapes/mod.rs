84aed2ebec69ae776230251ff362202fddf3c501
Nathan Ferry <nathan.ferry@live.fr>
2026-03-05 16:40:35 +0100

    export to scope specific function

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -5,3 +5,19 @@ mod rectangle;
 pub use shape::Shape;
 pub use circle::Circle;
 pub use rectangle::Rectangle;
+
+pub fn shape_main() {
+    let rect = Rectangle::new(4.0, 5.0);
+    let square = Rectangle::new(4.0, 4.0);
+    let circle = Circle::new(3.0);
+
+    println!("Is the rectangle a square? {}", rect.is_square());
+    println!("Rectangle area: {}", rect.area());
+    println!("Circle area: {}", circle.area());
+
+    let shapes: Vec<&dyn Shape> = vec![&rect, &square, &circle];
+
+    for shape in shapes {
+        println!("Shape is {}, area: {}", shape.name(), shape.area());
+    }
+}
